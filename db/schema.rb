051a5618d68e9fdfe70f8d516c6cfac66e51ae76
Nalipp <nateplipp@gmail.com>
2016-02-10 22:12:16 +0900

    add paperclip images

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160210120134) do
+ActiveRecord::Schema.define(version: 20160210123658) do
 
   create_table "items", force: :cascade do |t|
     t.string   "title"
@@ -19,8 +19,12 @@ ActiveRecord::Schema.define(version: 20160210120134) do
     t.integer  "price"
     t.integer  "shipping"
     t.boolean  "stock"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_uploaded_at"
   end
 
   create_table "users", force: :cascade do |t|
